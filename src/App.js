1a3428eca758f66a77ed7ea82b2d7434a0d60705
JacobR96 <jrosswalks@gmail.com>
2023-03-26 04:48:07 -0700

    Added react app to new foilder and pushed it to github, fixed the divs and the css and will be working on next page

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Thebody from './Components/Thebody';
+import Thenav from './Components/Thenav';
+
+
+
+
+function App() {
+  return (<>   
+     
+
+
+<Thenav/>
+<Thebody/>
+   </>
+  );
+}
+
+export default App;
